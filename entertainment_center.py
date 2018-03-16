0fb111d1b2563e3acb982bb6001a98102b65f765
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-03-15 23:19:51 -0300

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",                        "https://imgc.allpostersimages.com/img/print/posters/toy-story-woody-buzz_a-G-13390942-0.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
